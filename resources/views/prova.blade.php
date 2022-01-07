ffa06d2240e6703c38cc63c45260fb14b0065cef
Panajotc <panajot7@gmail.com>
2022-01-07 01:47:29 +0100

    fixes

--- resources/views/prova.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="https://kit.fontawesome.com/0a55ccbade.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
-
-<livewire:pictures imag="{{ URL::to('/images/nature.jpg') }}" name="Nature">
